26479d7094325dccd31dcefe5974268d466a318a
alexbochkarev <alexes.bochkarev@mail.ru>
2022-07-21 23:33:01 +0400

    Изучил документацию Models и создал модель для blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from  blog.models import Post
+from blog.models import Post
 
 #admin.site.register(Post)
 
 @admin.register(Post)
 class PageAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'date_created', 'data_updated', 'author']
+    prepopulated_fields = {'slug': ("title",)}
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from ckeditor.fields import RichTextField
 
+# https://docs.djangoproject.com/en/4.0/topics/db/models/
+
+# модель поста
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+
+    """
+    class - поддерживает методы,
+    модель содержит поля.
+    Поля имеют настройки поведения.
+    https://django-taggit.readthedocs.io/en/latest/
+    """
+
+    #  мета опции
+    class Meta:
+        verbose_name = 'Создать пост'
+        verbose_name_plural = 'Создать посты'
+
+    # поля модели
+    title = models.CharField(max_length=200,db_index=True, help_text='не более 200 символов') # настройки полей
+    #content = models.TextField(max_length=5000, blank=True, null=True, help_text='не более 5000 символов')
+    content = RichTextField(max_length=5000, blank=True, null=True, help_text='не более 5000 символов')
+    date_created = models.DateTimeField(default=timezone.now) # дата создания
+    data_updated = models.DateTimeField(auto_now=True) # дата обновления
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # в url при использовании slug обязательно добавлять id + get_absolute_url()
+    slug = models.SlugField(max_length=50)
+    likes = models.ManyToManyField(User, related_name='postcomment', blank=True)
+    reply = models.ForeignKey('self', null=True, related_name='reply_ok', on_delete=models.CASCADE)
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+    # методы модели
+    def __str__(self):
+        return self.title
