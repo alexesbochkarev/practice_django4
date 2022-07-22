692f4422239de12c20d967dc06b44b7c037f9ab9
alexbochkarev <alexes.bochkarev@mail.ru>
2022-07-23 00:30:50 +0400

    До конца разобрался с моделями и добавил новые поля

--- blog/models.py
+++ blog/models.py
@@ -11,9 +11,12 @@ class Post(models.Model):
 
     """
     class - поддерживает методы,
-    модель содержит поля.
+    модель содержит поля. https://django.fun/docs/django/ru/4.0/ref/forms/fields/
+    справочник типов полей. https://django.fun/docs/django/ru/4.0/ref/models/fields/#django.db.models.SlugField
     Поля имеют настройки поведения.
-    https://django-taggit.readthedocs.io/en/latest/
+
+
+    https://django-taggit.readthedocs.io/en/latest/ добавление тегов
     """
 
     #  мета опции
@@ -21,23 +24,23 @@ class Post(models.Model):
         verbose_name = 'Создать пост'
         verbose_name_plural = 'Создать посты'
 
-    # поля модели
-    title = models.CharField(max_length=200,db_index=True, help_text='не более 200 символов') # настройки полей
+    title = models.CharField(max_length=200,db_index=True, help_text='не более 200 символов', verbose_name='Заголовок')
     #content = models.TextField(max_length=5000, blank=True, null=True, help_text='не более 5000 символов')
-    content = RichTextField(max_length=5000, blank=True, null=True, help_text='не более 5000 символов')
-    date_created = models.DateTimeField(default=timezone.now) # дата создания
-    data_updated = models.DateTimeField(auto_now=True) # дата обновления
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = RichTextField(max_length=5000, blank=True, null=True, help_text='не более 5000 символов', verbose_name='Описание')
+    date_created = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
+    data_updated = models.DateTimeField(auto_now=True, verbose_name='Дата ообновления')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
     # в url при использовании slug обязательно добавлять id + get_absolute_url()
-    slug = models.SlugField(max_length=50)
-    likes = models.ManyToManyField(User, related_name='postcomment', blank=True)
-    reply = models.ForeignKey('self', null=True, related_name='reply_ok', on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=50,db_index=True, verbose_name='URL')
+    likes = models.ManyToManyField(User, related_name='postcomment', blank=True, verbose_name='Понравилось')
+    reply = models.ForeignKey('self', null=True, blank=True, related_name='reply_ok', on_delete=models.CASCADE, verbose_name='категории')
+    download = models.ImageField(upload_to='home/alex/Рабочий стол/', verbose_name='Загрузить фото')
 
     def total_likes(self):
         return self.likes.count()
 
     def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse('post-detail', kwargs={'post_slug': self.slug})
 
     # методы модели
     def __str__(self):
